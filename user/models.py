efc24e6b3fd98835372f93d8a3275a451548055a
Mykola Setrakov <nikset1@gmail.com>
2024-06-02 12:14:08 +0300

    create Follow model

--- user/models.py
+++ user/models.py
@@ -62,3 +62,10 @@ class User(AbstractUser):
 
     objects = UserManager()
 
+
+class Follow(models.Model):
+    follower = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
+    following = models.ForeignKey(User, related_name='followers', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('follower', 'following')
