50e3767418fae7714b5fbe09850bc6e413aeafef
Mykola Setrakov <nikset1@gmail.com>
2024-06-22 16:27:08 +0300

    register models to admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models import Post, Comment, Hashtag, Like
+
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Hashtag)
+admin.site.register(Like)
